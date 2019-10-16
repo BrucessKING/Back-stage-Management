7f4da76e6f0b15e8447bf8b984aeb98b8db45791
BrucessKING <39517489+BrucessKING@users.noreply.github.com>
2019-10-16 23:34:25 +0800

    Update addMessage.php

--- addMessage.php
+++ addMessage.php
@@ -1,21 +1,18 @@
 <?php
-
 	$nickname = @$_POST['nickname'];
 	$email = @$_POST['email'];
 	$content = @$_POST['content'];
 	$now_time = @$_POST['now_time'];
-	echo $content;
-	//echo "ok";
-	$con = @mysql_connect("localhost", "root", "");
+	$ini= @parse_ini_file("config.ini");
+    $con = @mysql_connect($ini["servername"],$ini["username"],$ini["password"]);
 	if($con){
 		mysql_query("set names 'utf8'");//解决中文乱码问题
-		mysql_select_db("student_manage");
+		mysql_select_db($ini["dbname"]);
 		$sql1 = "select count(*) from message_board";
 		$result = mysql_query($sql1);
 		$floor = mysql_fetch_row($result)[0] + 1;
 		$sql = "insert into message_board values($floor,\"$nickname\",\"$email\",\"$content\",\"$now_time\")";
 		mysql_query($sql);
-		//echo $sql;
 	}
 
 ?>
